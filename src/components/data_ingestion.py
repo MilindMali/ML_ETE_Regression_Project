74235ff045bb9c8ecb460afcd4f826e6e55821f5
Milind_Mali <milindmali108@gmail.com>
2024-05-30 00:01:31 +0530

    Data Transformation file completed.

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,7 +7,11 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.logger import logging
 from src.exception import CustomException
+from src.components.data_transformation import DataTransformation
 
+# src\components\data_ingestion.py
+
+@dataclass
 class DataIngestionConfig:
     train_data_path=TRAIN_FILE_PATH
     test_data_path=TEST_FILE_PATH
@@ -34,17 +38,24 @@ class DataIngestion:
             os.makedirs(os.path.dirname(self.data_ingestion_config.test_data_path),exist_ok=True)
             test_set.to_csv(self.data_ingestion_config.test_data_path,header=True)
 
+            logging.info("Data Ingestion is completed.")
+
             return (
                 self.data_ingestion_config.train_data_path,
                 self.data_ingestion_config.test_data_path
             )
-
+        
         except Exception as e:
             raise CustomException (e,sys)
         
 
 
+
 if __name__=="__main__":
     obj=DataIngestion()
-    train_data,test_data=obj.initiate_data_ingestion()
+    train_data_path,test_data_path=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    train_arr, test_arr,_=data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+
 
--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -10,11 +10,11 @@ CURRENT_TIME_STAMP=get_current_time_stamp()
 
 ROOT_DIR_KEY=os.getcwd()
 DATA_DIR='Dataset'
-DATA_DIR_KEY='delivery_data.csv'
+DATA_DIR_KEY='finalTrain.csv'
 
 ARTIFACT_DIR_KEY='Artifact'
 
-#Now we will define variable for data ingestion
+#Data ingestion related variable
 
 DATA_INGESTION_KEY='data_ingestion'
 DATA_INGESTION_RAW_DATA_DIR='raw_data_dir'  #downloaded data will be store here-->raw.csv
@@ -25,4 +25,16 @@ RAW_DATA_DIR_KEY='raw.csv'
 TRAIN_DATA_DIR_KEY='train.csv'
 TEST_DATA_DIR_KEY='test.csv'
 
+# Data transformation related variables
 
+DATA_TRANSFORMATION_ARTIFACT='data_transformation'
+DATA_PREPROCESSED_DIR='processor'
+DATA_TRANSFORMATION_PREPROCESSING_OBJ='processor.pkl'
+DATA_TRANSFORM_DIR='transformation'
+TRANSFORM_TRAIN_DIR_KEY='train.csv'
+TRANSFORM_TEST_DIR_KEY='test.csv'
+
+#inference
+#artifact/data_transformation/processor/processor.pkl
+#                            /transformation/train.csv
+#                                           / test.csv    
