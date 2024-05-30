769de298c99bc98e093eeb26d1835332c30c14c0
Milind_Mali <milindmali108@gmail.com>
2024-05-30 07:41:22 +0530

    Minor changes in Data Transformation file.

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -12,14 +12,13 @@ from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler, OrdinalEncoder,OneHotEncoder
 from sklearn.pipeline import Pipeline
 from src.utils import save_obj
-# from src.config.configuration import PREPROCESING_OBJ_FILE,TRANSFORM_TRAIn_FILE_PATH,TRANSFORM_TEST_FILE_PATH,FEATURE_ENGG_OBJ_FILE_PATH
 
 # FE
 # Data transformation
 
 class Feature_Engineering(BaseEstimator, TransformerMixin):
     def __init__(self):
-        logging.info("******************feature Engineering started******************")
+        logging.info("feature Engineering started.")
 
     def distance_numpy(self, df, lat1, lon1,lat2, lon2 ):
         p = np.pi/180
@@ -40,8 +39,6 @@ class Feature_Engineering(BaseEstimator, TransformerMixin):
                                 'Delivery_location_longitude',
                                 'Order_Date','Time_Orderd','Time_Order_picked'], axis=1,inplace=True)
             
-            logging.info("droping columns from our original dataset")
-            
             return df
 
         except Exception as e:
@@ -61,7 +58,6 @@ class Feature_Engineering(BaseEstimator, TransformerMixin):
 @dataclass 
 class DataTransformationConfig():
     proccessed_obj_file_path = PREPROCESSING_OBJ_FILE
-
     transform_train_path = TRANSFORM_TRAIN_FILE_PATH
     transform_test_path = TRANSFORM_TEST_FILE_PATH
     feature_engg_obj_path = FEATURE_ENGG_OBJ_FILE_PATH
@@ -103,14 +99,15 @@ class DataTransformation:
             ])
 
 
-            preprocssor = ColumnTransformer([
+            preprocessor = ColumnTransformer([
                 ('numerical_pipeline', numerical_pipeline,numerical_column ),
                 ('categorical_pipeline', categorical_pipeline,categorical_columns ),
                 ('ordinal_pipeline', ordinal_pipeline,ordinal_encoder )
             ])
 
             logging.info("Pipeline Steps Completed")
-            return preprocssor
+
+            return preprocessor
 
         except Exception as e:
             raise CustomException( e,sys)
@@ -120,17 +117,19 @@ class DataTransformation:
         try:
             feature_engineering = Pipeline(steps = [("fe",Feature_Engineering())])
 
+            logging.info("feature engineering is completed")
+
             return feature_engineering
 
         except Exception as e:
             raise CustomException( e,sys)
         
     def initiate_data_transformation(self, train_path, test_path):
+        logging.info("Data transformation initiated.")
         try:
             train_df = pd.read_csv(train_path)
             test_df = pd.read_csv(test_path)
 
-            logging.info("Optaining FE steps object")
             fe_obj = self.get_feature_engineering_object()
 
             train_df = fe_obj.fit_transform(train_df)
@@ -140,8 +139,7 @@ class DataTransformation:
             train_df.to_csv("train_data.csv")
             test_df.to_csv("test_data.csv")
 
-
-            processinf_obj = self.get_data_transformation_obj()
+            processing_obj = self.get_data_transformation_obj()
 
             traget_columns_name = "Time_taken (min)"
 
@@ -151,8 +149,10 @@ class DataTransformation:
             X_test = test_df.drop(columns = traget_columns_name, axis = 1)
             y_test = test_df[traget_columns_name]
 
-            X_train = processinf_obj.fit_transform(X_train)
-            X_test = processinf_obj.transform(X_test)
+            X_train = processing_obj.fit_transform(X_train)
+            X_test = processing_obj.transform(X_test)
+
+            logging.info("Data successfully transformed.")
 
             train_arr = np.c_[X_train, np.array(y_train)]
             test_arr = np.c_[X_test, np.array(y_test)]
@@ -160,13 +160,14 @@ class DataTransformation:
             df_train = pd.DataFrame(train_arr)
             df_test = pd.DataFrame(test_arr)
 
+
             os.makedirs(os.path.dirname(self.data_transformation_config.transform_train_path), exist_ok=True)
             df_train.to_csv(self.data_transformation_config.transform_train_path, index = False, header = True)
 
             os.makedirs(os.path.dirname(self.data_transformation_config.transform_test_path), exist_ok=True)
             df_test.to_csv(self.data_transformation_config.transform_test_path, index = False, header = True)
 
-
+            logging.info("New dataframe generated for train and test and saved as csv")
 
             save_obj(file_path = self.data_transformation_config.proccessed_obj_file_path,
                      obj = fe_obj)
@@ -174,9 +175,13 @@ class DataTransformation:
             save_obj(file_path = self.data_transformation_config.feature_engg_obj_path,
                      obj = fe_obj)
             
+            logging.info("Data Transformation phase completed.")
+            
             return(train_arr,
                    test_arr,
                    self.data_transformation_config.proccessed_obj_file_path)
+        
+            
         
         except Exception as e:
             raise CustomException( e,sys)
